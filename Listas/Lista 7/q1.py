c0444c2f546e7872196d58c3b0fca061ce42863e
Caio Costa <nextc3@gmail.com>
2021-05-29 19:09:37 -0300

    Questão 1 feitsa

--- Listas/Lista 7/q1.py
+++ Listas/Lista 7/q1.py
@@ -1,3 +1,12 @@
 
-nome = input("olá escreva seu nome: ")
-print(nome)
+#1. Entrar como n's números e imprimir o triplo de cada. o programa encerra quando entrar com o numero 999;
+
+
+
+
+numero = int(input("Entre com o numero: "))
+while numero != 999:
+    triplo = numero * 3
+    print("Triplo do números: {}".format(triplo))
+    numero = int(input("Entre com o numero: "))
+print("Programa encerrado")
