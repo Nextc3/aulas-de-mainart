2a5bb6188c64b7239a651afddd17a9dd50102981
Caio Costa <nextc3@gmail.com>
2021-05-29 21:43:39 -0300

    Colocando só o enunciado das questões

---
+++ Listas/Lista 7/q10.py
@@ -0,0 +1,6 @@
+'''
+10.Criar um
+programa que deixe entrar com 10 números positivos e imprima a raiz
+quadrada de cada numero. para cada entrada de dados devera haver um
+trecho de "proteção" para que um numero negativo não seja aceito.
+'''
