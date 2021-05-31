1f89e8db2dc67ddc12e196b3b01b53b8d28a0bd3
Caio Costa <nextc3@gmail.com>
2021-05-30 22:00:47 -0300

    Consigo compilar questão 9

--- Listas/Lista 7/q9.py
+++ Listas/Lista 7/q9.py
@@ -11,3 +11,54 @@ para cada consumidor; b)o total de consumo para os 3(três) tipos de
 consumidor; c)a media de consumo dos tipos 1 e 2.
 
 '''
+
+
+listaConsumidores = []
+consumidor = {
+    "consumo": 0,
+    "tipo": 0,
+    "numero": -1,
+}
+
+consumoBruto = 0
+
+quantidadeResidencial = 0
+quantidadeComercial = 0
+mediaResidencial = 0
+mediaComercial = 0
+
+totalAcumuladoComercial = 0
+totalAcumuladoResidencia = 0
+totalAcumuladoIndustrial = 0
+numero = -1
+while numero != 0:
+    
+    numero = int(input("Entre com o número do consumidor: "))
+    if (numero == 0):
+        break
+    consumidor["numero"] = numero
+    consumoBruto = int(input("Entre com o consumo: "))
+    consumidor["tipo"] = int(input("Entre com o tipo: "))
+    if (consumidor["tipo"] == 1):
+        consumidor["consumo"] = consumoBruto * 0.3
+        totalAcumuladoResidencia += consumidor["consumo"]
+        quantidadeResidencial += 1
+    elif (consumidor["tipo"] == 2):
+        consumidor["consumo"] = consumoBruto * 0.5
+        totalAcumuladoComercial += consumidor["consumo"]
+        quantidadeComercial += 1
+    elif (consumidor["tipo"] == 3):
+        consumidor["consumo"] = consumoBruto * 0.7 
+        totalAcumuladoIndustrial += consumidor["consumo"]
+
+    print("Total de consumo: {}".format(consumidor["consumo"]))
+    listaConsumidores.append(consumidor)
+    
+    
+    
+print("Tamanho da lista de consumidores: {}".format(len(listaConsumidores)))    
+mediaResidencial = totalAcumuladoResidencia / quantidadeResidencial
+mediaComercial = totalAcumuladoComercial / quantidadeComercial
+print("Total de consumo: Residencial = {}\nComercial = {}\nIndustrial = {}".format(totalAcumuladoResidencia,totalAcumuladoComercial,totalAcumuladoIndustrial))
+
+print("Media de consumo: Residencial = {}\nCcomercial = {}".format(mediaResidencial,mediaComercial))
