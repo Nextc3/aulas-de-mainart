76c98d790f3466bc895c774c2e81b2abbafcc978
Caio Costa <nextc3@gmail.com>
2021-05-31 21:07:53 -0300

    Questão 10 feita

--- Listas/Lista 7/q10.py
+++ Listas/Lista 7/q10.py
@@ -4,3 +4,14 @@ programa que deixe entrar com 10 números positivos e imprima a raiz
 quadrada de cada numero. para cada entrada de dados devera haver um
 trecho de "proteção" para que um numero negativo não seja aceito.
 '''
+
+
+#import math
+
+
+for _ in range(10):
+    numero = int(input("Entre com o número: "))
+    if(numero < 0):
+        print("Número digitado deve ser positivo")
+        continue
+    print("Raiz quadrada do número digitado: {}".format(numero ** (1/2)))
